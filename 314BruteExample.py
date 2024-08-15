1082f177bd166e39082d77f6eceacd274aa95a73
spaceybread <spaceybread@gmail.com>
2024-08-15 17:27:15 +0530

    Updated brute example

--- 314BruteExample.py
+++ 314BruteExample.py
@@ -8,14 +8,26 @@ lat = [
     [0, 1, 1, 3]
 ]
 
-fe = FuzzyExtractor(lat, 4)
-s, e = fe.gen(314)
+all = set()
 
-for i in range(9):
-    for j in range(9):
-        for k in range(9):
-            n = i * 100 + j * 10 + k
-            ep = fe.recov(s, n)
-            if np.array_equal(e, ep):
-                print(n, e, ep, True)
+for _ in range(1000):
+    # init at different instances results in some
+    # differences, which is expected since 
+    # FuzzyExtractor picks random coeffs each time
+    # it is init, as such, some values are considered
+    # similar sometimes while not close other times
+
+    fe = FuzzyExtractor(lat, 4)
+    s, e = fe.gen(314)
+
+    for i in range(9):
+        for j in range(9):
+            for k in range(9):
+                n = i * 100 + j * 10 + k
+                ep = fe.recov(s, n)
+                if np.array_equal(e, ep):
+                    if n not in all:
+                        print(_, n)
+                        all.add(n)
             
+print(sorted(list(all)))
