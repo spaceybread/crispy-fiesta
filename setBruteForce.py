332918be48969dd0c0b2c63448e453c3b69cf8aa
spaceybread <spaceybread@gmail.com>
2024-08-23 23:19:17 +0530

    Added debug and verified the results

--- setBruteForce.py
+++ setBruteForce.py
@@ -2,24 +2,28 @@ from pyFuzzImpl import FuzzyExtractor
 from furbyMatchingImpl import FuzzyMatching
 import numpy as np
 
-# weird code, unexpected behaviour 
-
 LATTICE = [[1, 0, -1, 2],[2, 3, 1, -1],[1, 1, 2, 0], [0, 1, 1, 3]]
 
 out = []
-for _ in range(10):
-    FEZ = FuzzyExtractor(LATTICE, 4)
-    FM = FuzzyMatching(FEZ)
 
-    S_A = [a for a in range(1000)]
-    S_B = [314]
+FEZ = FuzzyExtractor(LATTICE, 4)
+FM = FuzzyMatching(FEZ)
 
-    keys, helpers = FM.makeHelpers(S_A)
-    rec = FM.attemptMatching(helpers, S_B)
-    final = FM.returnMatches(rec, S_A, keys)
+S_A = [314, 168, 159, 333, 819]
+S_B = [i for i in range(1000)]
+    
 
-    for a in final:
-        if a not in out: 
-            out.append((a, _))
+keys, helpers = FM.makeHelpers(S_A)
+rec = FM.attemptMatching(helpers, S_B)
 
-print(sorted(out))
+# this is a debug version of FM.returnMatches(rec, S_A, keys)
+# that returns pairs that were matched
+# this is never really be possible because A should
+# never have access to S_B
+finalDebug = FM.returnMatchesAsPairs(rec, S_A, keys, S_B)
+final = FM.returnMatches(rec, S_A, keys)
+
+for l in finalDebug:
+    print(*l)
+
+print(final)
