9339b1fec46d8bb938bd6772a40ae9d2a151aa95
spaceybread <spaceybread@gmail.com>
2024-09-17 13:51:00 -0500

    Gauss lattice work

--- fuzzyGaussBrute.py
+++ fuzzyGaussBrute.py
@@ -1,11 +1,5 @@
-from pyFuzzImpl import FuzzyExtractor
+from pyFuzzImpl import GaussFuzzyExtractor
 import numpy as np
-
-lat = []
-
-for i in range(24):
-    line = [0] * i + [2] + [0] * (23 - i)
-    lat.append(line)
 
 # Gaussian Lattice with scale 2 and 24 dimensions
 
@@ -16,6 +10,7 @@ all = set()
 
 GEN_NUM = 314
 #GEN_NUM = int(input())
+fe = GaussFuzzyExtractor(16, 2)
 
 for _ in range(10):
     # init at different instances results in some
@@ -24,12 +19,11 @@ for _ in range(10):
     # it is init, as such, some values are considered
     # similar sometimes while not close other times
 
-    fe = FuzzyExtractor(lat, 24)
     s, e = fe.gen(GEN_NUM)
 
-    for i in range(9):
-        for j in range(9):
-            for k in range(9):
+    for i in range(10):
+        for j in range(10):
+            for k in range(10):
                 n = i * 100 + j * 10 + k
                 ep = fe.recov(s, n)
                 if np.array_equal(e, ep):
@@ -38,3 +32,4 @@ for _ in range(10):
                         all.add(n)
             
 print(sorted(list(all)))
+print(len(all))
--- pyFuzzImpl.py
+++ pyFuzzImpl.py
@@ -61,6 +61,72 @@ class FuzzyExtractor:
         # point
          
         return e_final
+    
+class GaussFuzzyExtractor(FuzzyExtractor): 
+    def __init__(self, lattice_dim, scale, k = 9):
+        # the lattice is defined by a 2D numpy
+        # array [dim x dim] where lattice[i] 
+        # represents a basis vector
+        self.dim = lattice_dim
+        self.scale = scale
+        self.k = k
+        
+        lat = []
+        for i in range(lattice_dim):
+            line = [0] * i + [scale] + [0] * (lattice_dim - 1 - i)
+            lat.append(line)
+
+        self.lattice = lat
+
+    # take a point in R^n and find the closest
+    # lattice point
+    def closest(self, p, B):
+        z = np.round(np.linalg.solve(B, p))
+        return B @ z
+    
+
+    # get the vector and the snapped point
+    def gen(self, w):
+        es = np.zeros(self.dim, dtype=int)
+        ws = np.zeros(self.dim, dtype=int)
+
+        # there's probably a clean way to
+        # do this with numpy
+        for i in range(self.dim):
+            for j in range(self.dim):
+                c = rdm.randint(1, self.k)
+                es[j] += self.lattice[i][j] * c
+        
+        es = self.closest(es, self.lattice)
+        # after finding a random point in R^n, 
+        # snap it to some point in the lattice
+
+        for i in range(self.dim):
+            ws[i] = w % 10
+            w = w // 10
+        ws = ws[::-1]
+
+        bs = es - ws 
+        return bs, es
+
+    # given some point and the vector, find
+    # the closest lattice point
+    def recov(self, bs, w_p):
+        e_prime = np.zeros(self.dim, dtype=int)
+        
+        ws = np.zeros(self.dim, dtype=int)
+        for i in range(self.dim):
+            ws[i] = w_p % 10
+            w_p = w_p // 10
+        ws = ws[::-1]
+
+        e_prime = ws + bs
+        e_final = self.closest(e_prime, self.lattice)
+        # after finding the corresponding
+        # point, snap it to a lattice
+        # point
+         
+        return e_final
 
 class LeechFuzzyExtractor(FuzzyExtractor):
     def __init__(self, k):
