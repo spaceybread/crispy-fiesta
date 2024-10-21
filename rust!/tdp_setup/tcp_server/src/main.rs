4f51b07d7a96cb8f7c85e645da72f02320013fce
spaceybread <spaceybread@gmail.com>
2024-10-21 12:37:50 -0500

    TDP work

--- rust!/tdp_setup/tcp_server/src/main.rs
+++ rust!/tdp_setup/tcp_server/src/main.rs
@@ -4,17 +4,30 @@ use std::thread;
 
 fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 512]; 
-    match stream.read(&mut buffer) {
-        Ok(bytes_read) => {
-            println!("Received: {}", String::from_utf8_lossy(&buffer[..bytes_read]));
-            
-            let response = b"Message received\n";
-            if let Err(e) = stream.write_all(response) {
-                eprintln!("Failed to send response: {}", e);
+    while true {
+        match stream.read(&mut buffer) {
+            Ok(bytes_read) => {
+                let rec = String::from_utf8_lossy(&buffer[..bytes_read]);
+                if rec.trim().is_empty() {
+                    println!("Connection closed!");
+                    let response = b"Connection closed!\n";
+                    if let Err(e) = stream.write_all(response) {
+                        eprintln!("Failed to send response: {}", e);
+                    }
+                    return;
+                }
+
+                println!("Received: {}", rec);
+                let response = b"Message received\n";
+                if let Err(e) = stream.write_all(response) {
+                    eprintln!("Failed to send response: {}", e);
+                }
             }
-        }
-        Err(e) => {
-            eprintln!("Failed to read from stream: {}", e);
+            
+            Err(e) => {
+                eprintln!("Failed to read from stream: {}", e);
+                return;
+            }
         }
     }
 }
